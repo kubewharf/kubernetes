b97741a69fc1066d85a9490f9fc091a47c75b133
renjingsi <renjingsi@bytedance.com>
2021-04-08 11:55:19 +0800

    tidy common util functions for godel pod
    Change-Id: Iebb0cf15f683a3f1757329a13aac8784dac9d9b4

--- plugin/pkg/admission/godel/podannotations/admission.go
+++ plugin/pkg/admission/godel/podannotations/admission.go
@@ -3,50 +3,17 @@ package podannotations
 import (
 	"context"
 	"io"
+
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apiserver/pkg/admission"
 	api "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/plugin/pkg/admission/godel"
 )
 
 const (
 	// PluginName indicates name of admission plugin.
 	PluginName = "GodelPodAnnotations"
-
-	// PodStateAnnotationKey is a pod annotation key, value is the pod state
-	PodStateAnnotationKey = "godel.bytedance.com/pod-state"
-
-	// PodResourceTypeAnnotationKey is a pod annotation key, value is the pod resource type (guaranteed or best-effort)
-	PodResourceTypeAnnotationKey = "godel.bytedance.com/pod-resource-type"
-
-	// PodLauncherAnnotationKey is a pod annotation key, value is the launcher of this pod (kubelet or node-manager)
-	PodLauncherAnnotationKey = "godel.bytedance.com/pod-launcher"
-
-	// SchedulerAnnotationKey is a pod annotation key, value is the scheduler id who is responsible for scheduling this pod
-	SchedulerAnnotationKey = "selectedScheduler"
-
-	// AssumedNodeAnnotationKey is a pod annotation key, value is the assumed node name chosen by one scheduler
-	// the scheduler will reserve the allocated resource for the pod.
-	AssumedNodeAnnotationKey = "assumedNode"
-
-	// NominatedNodeAnnotationKey is a pod annotation key,
-	// value is the node name chosen by scheduler for placing the pending pod by evicting others
-	// value can be like: {node: node1, victims: pod1, pod2...}
-	// the scheduler will reserve the allocated resource for the pod.
-	NominatedNodeAnnotationKey = "nominatedNode"
-
-	// pod launchers
-	PodLauncherKubelet     = "kubelet"
-	PodLauncherNodeManager = "node-manager"
-
-	// pod resource types
-	GuaranteedPod = "guaranteed"
-	BestEffortPod = "best-effort"
-
-	// pod states
-	PodPending    = "Pending"
-	PodDispatched = "Dispatched"
-	PodAssumed    = "Assumed"
 )
 
 // Register registers a plugin
@@ -101,12 +68,12 @@ func (p *godelPodAnnotationsPlugin) Admit(ctx context.Context, a admission.Attri
 }
 
 func (p *godelPodAnnotationsPlugin) admitPodLauncher(pod *apiv1.Pod) error {
-	switch pod.Annotations[PodLauncherAnnotationKey] {
-	case PodLauncherKubelet, PodLauncherNodeManager:
+	switch godel.GetPodLauncher(pod) {
+	case godel.PodLauncherKubelet, godel.PodLauncherNodeManager:
 		return nil
 	case "":
 		// set default launcher as kubelet
-		setPodAnnotation(pod, PodLauncherAnnotationKey, PodLauncherKubelet)
+		godel.SetPodLauncher(pod, godel.PodLauncherKubelet)
 		return nil
 	default:
 		return errors.NewBadRequest("invalid pod launcher")
@@ -114,12 +81,12 @@ func (p *godelPodAnnotationsPlugin) admitPodLauncher(pod *apiv1.Pod) error {
 }
 
 func (p *godelPodAnnotationsPlugin) admitPodResourceType(pod *apiv1.Pod) error {
-	switch pod.Annotations[PodResourceTypeAnnotationKey] {
-	case GuaranteedPod, BestEffortPod:
+	switch godel.GetPodResourceType(pod) {
+	case godel.GuaranteedPod, godel.BestEffortPod:
 		return nil
 	case "":
 		// set default resource type as guaranteed
-		setPodAnnotation(pod, PodResourceTypeAnnotationKey, GuaranteedPod)
+		godel.SetPodResourceType(pod, godel.GuaranteedPod)
 		return nil
 	default:
 		return errors.NewBadRequest("invalid pod resource type")
@@ -127,104 +94,11 @@ func (p *godelPodAnnotationsPlugin) admitPodResourceType(pod *apiv1.Pod) error {
 }
 
 func (p *godelPodAnnotationsPlugin) admitPodState(pod *apiv1.Pod) error {
-	if AbnormalPodState(pod) {
+	if godel.AbnormalPodState(pod) {
 		return errors.NewBadRequest("godel pod state is abnormal")
 	}
-	if PendingPod(pod) && len(pod.Annotations[PodStateAnnotationKey]) == 0 {
-		setPodAnnotation(pod, PodStateAnnotationKey, PodPending)
+	if godel.PendingPod(pod) && len(godel.GetPodState(pod)) == 0 {
+		godel.SetPodState(pod, godel.PodPending)
 	}
 	return nil
-}
-
-func setPodAnnotation(pod *apiv1.Pod, key, value string) {
-	if pod.Annotations == nil {
-		pod.Annotations = make(map[string]string)
-	}
-	pod.Annotations[key] = value
-}
-
-// PendingPod checks if the given pod is in pending state
-func PendingPod(pod *apiv1.Pod) bool {
-	if pod.Annotations != nil &&
-		(pod.Annotations[PodStateAnnotationKey] == PodPending || len(pod.Annotations[PodStateAnnotationKey]) == 0) &&
-		len(pod.Annotations[SchedulerAnnotationKey]) == 0 &&
-		len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 &&
-		len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 &&
-		len(pod.Spec.NodeName) == 0 {
-		return true
-	}
-	return false
-}
-
-// DispatchedPod checks if the given pod is in dispatched state
-func DispatchedPod(pod *apiv1.Pod) bool {
-	if pod.Annotations != nil &&
-		pod.Annotations[PodStateAnnotationKey] == PodDispatched &&
-		len(pod.Annotations[SchedulerAnnotationKey]) != 0 &&
-		len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 &&
-		len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 &&
-		len(pod.Spec.NodeName) == 0 {
-		return true
-	}
-	return false
-}
-
-// assumedOrNominatedNodeIsSet checks if the AssumedNodeAnnotationKey or NominatedNodeAnnotationKey is set
-func assumedOrNominatedNodeIsSet(pod *apiv1.Pod) bool {
-	if pod.Annotations != nil {
-		if len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 && len(pod.Annotations[NominatedNodeAnnotationKey]) != 0 {
-			return true
-		}
-		if len(pod.Annotations[AssumedNodeAnnotationKey]) != 0 && len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 {
-			return true
-		}
-	}
-	return false
-}
-
-// AssumedPod checks if the given pod is in assumed state
-func AssumedPod(pod *apiv1.Pod) bool {
-	if pod.Annotations != nil &&
-		pod.Annotations[PodStateAnnotationKey] == PodAssumed &&
-		len(pod.Annotations[SchedulerAnnotationKey]) != 0 &&
-		assumedOrNominatedNodeIsSet(pod) &&
-		len(pod.Spec.NodeName) == 0 {
-		return true
-	}
-	return false
-}
-
-// BoundPod checks if the given pod is bound
-func BoundPod(pod *apiv1.Pod) bool {
-	return len(pod.Spec.NodeName) != 0
-}
-
-// AbnormalPodState checks if the given pod is in abnormal state
-func AbnormalPodState(pod *apiv1.Pod) bool {
-	if BoundPod(pod) {
-		return false
-	}
-
-	switch pod.Annotations[PodStateAnnotationKey] {
-	case "", PodPending:
-		if !PendingPod(pod) {
-			return true
-		} else {
-			return false
-		}
-	case PodDispatched:
-		if !DispatchedPod(pod) {
-			return true
-		} else {
-			return false
-		}
-	case PodAssumed:
-		if !AssumedPod(pod) {
-			return true
-		} else {
-			return false
-		}
-	default:
-		return true
-	}
 }
---
+++ plugin/pkg/admission/godel/util.go
@@ -0,0 +1,159 @@
+package godel
+
+import (
+	apiv1 "k8s.io/api/core/v1"
+)
+
+const (
+	// PodStateAnnotationKey is a pod annotation key, value is the pod state
+	PodStateAnnotationKey = "godel.bytedance.com/pod-state"
+
+	// PodResourceTypeAnnotationKey is a pod annotation key, value is the pod resource type (guaranteed or best-effort)
+	PodResourceTypeAnnotationKey = "godel.bytedance.com/pod-resource-type"
+
+	// PodLauncherAnnotationKey is a pod annotation key, value is the launcher of this pod (kubelet or node-manager)
+	PodLauncherAnnotationKey = "godel.bytedance.com/pod-launcher"
+
+	// SchedulerAnnotationKey is a pod annotation key, value is the scheduler id who is responsible for scheduling this pod
+	SchedulerAnnotationKey = "selectedScheduler"
+
+	// AssumedNodeAnnotationKey is a pod annotation key, value is the assumed node name chosen by one scheduler
+	// the scheduler will reserve the allocated resource for the pod.
+	AssumedNodeAnnotationKey = "assumedNode"
+
+	// NominatedNodeAnnotationKey is a pod annotation key,
+	// value is the node name chosen by scheduler for placing the pending pod by evicting others
+	// value can be like: {node: node1, victims: pod1, pod2...}
+	// the scheduler will reserve the allocated resource for the pod.
+	NominatedNodeAnnotationKey = "nominatedNode"
+
+	// pod launchers
+	PodLauncherKubelet     = "kubelet"
+	PodLauncherNodeManager = "node-manager"
+
+	// pod resource types
+	GuaranteedPod = "guaranteed"
+	BestEffortPod = "best-effort"
+
+	// pod states
+	PodPending    = "Pending"
+	PodDispatched = "Dispatched"
+	PodAssumed    = "Assumed"
+)
+
+func GetPodLauncher(pod *apiv1.Pod) string {
+	return pod.Annotations[PodLauncherAnnotationKey]
+}
+
+func SetPodLauncher(pod *apiv1.Pod, launcher string) {
+	setPodAnnotation(pod, PodLauncherAnnotationKey, launcher)
+}
+
+func GetPodResourceType(pod *apiv1.Pod) string {
+	return pod.Annotations[PodResourceTypeAnnotationKey]
+}
+
+func SetPodResourceType(pod *apiv1.Pod, resourceType string) {
+	setPodAnnotation(pod, PodResourceTypeAnnotationKey, resourceType)
+}
+
+func GetPodState(pod *apiv1.Pod) string {
+	return pod.Annotations[PodStateAnnotationKey]
+}
+
+func SetPodState(pod *apiv1.Pod, state string) {
+	setPodAnnotation(pod, PodStateAnnotationKey, state)
+}
+
+func setPodAnnotation(pod *apiv1.Pod, key, value string) {
+	if pod.Annotations == nil {
+		pod.Annotations = make(map[string]string)
+	}
+	pod.Annotations[key] = value
+}
+
+// PendingPod checks if the given pod is in pending state
+func PendingPod(pod *apiv1.Pod) bool {
+	if pod.Annotations != nil &&
+		(pod.Annotations[PodStateAnnotationKey] == PodPending || len(pod.Annotations[PodStateAnnotationKey]) == 0) &&
+		len(pod.Annotations[SchedulerAnnotationKey]) == 0 &&
+		len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 &&
+		len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 &&
+		len(pod.Spec.NodeName) == 0 {
+		return true
+	}
+	return false
+}
+
+// DispatchedPod checks if the given pod is in dispatched state
+func DispatchedPod(pod *apiv1.Pod) bool {
+	if pod.Annotations != nil &&
+		pod.Annotations[PodStateAnnotationKey] == PodDispatched &&
+		len(pod.Annotations[SchedulerAnnotationKey]) != 0 &&
+		len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 &&
+		len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 &&
+		len(pod.Spec.NodeName) == 0 {
+		return true
+	}
+	return false
+}
+
+// assumedOrNominatedNodeIsSet checks if the AssumedNodeAnnotationKey or NominatedNodeAnnotationKey is set
+func assumedOrNominatedNodeIsSet(pod *apiv1.Pod) bool {
+	if pod.Annotations != nil {
+		if len(pod.Annotations[AssumedNodeAnnotationKey]) == 0 && len(pod.Annotations[NominatedNodeAnnotationKey]) != 0 {
+			return true
+		}
+		if len(pod.Annotations[AssumedNodeAnnotationKey]) != 0 && len(pod.Annotations[NominatedNodeAnnotationKey]) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// AssumedPod checks if the given pod is in assumed state
+func AssumedPod(pod *apiv1.Pod) bool {
+	if pod.Annotations != nil &&
+		pod.Annotations[PodStateAnnotationKey] == PodAssumed &&
+		len(pod.Annotations[SchedulerAnnotationKey]) != 0 &&
+		assumedOrNominatedNodeIsSet(pod) &&
+		len(pod.Spec.NodeName) == 0 {
+		return true
+	}
+	return false
+}
+
+// BoundPod checks if the given pod is bound
+func BoundPod(pod *apiv1.Pod) bool {
+	return len(pod.Spec.NodeName) != 0
+}
+
+// AbnormalPodState checks if the given pod is in abnormal state
+func AbnormalPodState(pod *apiv1.Pod) bool {
+	if BoundPod(pod) {
+		return false
+	}
+
+	switch pod.Annotations[PodStateAnnotationKey] {
+	case "", PodPending:
+		if !PendingPod(pod) {
+			return true
+		} else {
+			return false
+		}
+	case PodDispatched:
+		if !DispatchedPod(pod) {
+			return true
+		} else {
+			return false
+		}
+	case PodAssumed:
+		if !AssumedPod(pod) {
+			return true
+		} else {
+			return false
+		}
+	default:
+		return true
+	}
+}
