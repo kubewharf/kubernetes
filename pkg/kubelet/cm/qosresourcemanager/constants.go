d37102718a67602ee9e2e6d32568c819c0a213d7
孙健俞 <sunjianyu@bytedance.com>
2022-04-13 10:46:24 +0800

    QRM skip normal daemonset

--- pkg/kubelet/cm/qosresourcemanager/constants.go
+++ pkg/kubelet/cm/qosresourcemanager/constants.go
@@ -3,4 +3,5 @@ package qosresourcemanager
 const (
 	PSMLabelKey                    = "psm"
 	MainContainerNameAnnotationKey = "bytedance.com/main-container-name"
+	DaemonsetKind                  = "DaemonSet"
 )
